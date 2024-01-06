9323ae0c8e8764fe2f8c9fb2078f51d1b5f1df4d
Rishikesh-Rahane <rahanerishikesh63@gmail.com>
2024-01-06 23:06:10 +0530

    Initial commit from Create Next App

---
+++ src/app/layout.js
@@ -0,0 +1,28 @@
+import { Inter } from "next/font/google";
+import { ToastContainer } from "react-toastify";
+import "./styles/App.css";
+import 'react-toastify/dist/ReactToastify.css';
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Internship Page",
+  description: "Rablo Innovate Internship Page",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&family=Roboto:ital,wght@0,100;0,300;1,100;1,300&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body className={inter.className}>{children}
+          <ToastContainer/>
+          </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,28 @@
+import { Poppins } from "next/font/google";
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["300"],
+  display: "swap",
+});
+import Navbar from "./components/Navbar/Navbar";
+import Apply from "./components/apply/Apply";
+import Profile from "./components/profiles_section/Profile";
+import "./styles/App.css";
+
+export default function Home() {
+  return (
+    <main className={poppins.className}>
+      <div className="App">
+        <div className="navbar">
+          <Navbar></Navbar>
+        </div>
+        <div className="Apply">
+          <Apply></Apply>
+        </div>
+        <div className="Profile" style={{ color: "black" }}>
+          <Profile></Profile>
+        </div>
+      </div>
+    </main>
+  );
+}
